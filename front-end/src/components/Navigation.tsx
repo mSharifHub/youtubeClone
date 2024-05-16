6200923c58fc911490da33a5c575bfa777d2f4fb
Mohamed Sharif <135287156+mSharifHub@users.noreply.github.com>
2024-05-16 03:51:31 -0700

    changes

---
+++ front-end/src/components/Navigation.tsx
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+export default function NavigationBar() {
+  return (
+    <div className="grid grid-cols-[0.5fr_1fr_0.5fr] h-20 border-2">
+      <div className="col-span-1 col-start-1 flex justify-start items-center border-2">
+        <div className="flex justify-center items-center mx-4">
+          <FontAwesomeIcon icon={faBars} size="2xl" className="mr-8" />
+          <FontAwesomeIcon icon={faYoutube} className="text-3xl text-red-600" />
+          <span className="text-3xl font-black capitalize p-1 ">Youtube</span>
+        </div>
+      </div>
+    </div>
+  );
+}
