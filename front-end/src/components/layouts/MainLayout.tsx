609a153dfde1da0c4b7aa86aec89c4403a9ca821
Mohamed Sharif <135287156+mSharifHub@users.noreply.github.com>
2024-05-19 00:50:28 -0700

    fix

--- front-end/src/components/layouts/MainLayout.tsx
+++ front-end/src/components/layouts/MainLayout.tsx
@@ -6,17 +6,19 @@ import RecommendationsFilters from '../RecommendationsFilters.tsx';
 
 export default function MainLayout() {
   return (
-    <div className=" h-screen w-screen flex flex-col  px-4 py-4">
+    <div className=" min-h-screen min-w-screen flex flex-col  px-4 py-4 ">
       <NavigationBar />
       <main className="h-full w-full flex flex-row ">
         <MenuBar />
-        <div className=" flex flex-col flex-grow overflow-hidden">
+        <div className=" flex flex-col flex-grow  overflow-hidden ">
           {/* Scroll-x  MLL suggestions container */}
-          <div className="relative">
+          <div className="relative ">
+            {/* fading effect*/}
             <div className="absolute inset-y-0 left-0 w-10 bg-gradient-to-r from-white to-transparent pointer-events-none z-10" />
             <ScrollContainerHorizontal>
               <RecommendationsFilters />
             </ScrollContainerHorizontal>
+            {/* fading effect*/}
             <div className="absolute inset-y-0 right-0 w-10 bg-gradient-to-l from-white to-transparent pointer-events-none z-10" />
           </div>
           <Outlet />
