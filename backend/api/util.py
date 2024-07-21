da5bf2b0de62f5e95c6705bf58fb3f5d46ed9df8
Mohamed Sharif <135287156+mSharifHub@users.noreply.github.com>
2024-07-21 05:20:03 -0700

    added additional instances for api end point to get code from google api

--- backend/api/util.py
+++ backend/api/util.py
@@ -1,31 +1,27 @@
-import os.path
-
-from oauth2client import client
+# import os.path
+# from oauth2client import client
 
 from django.conf import settings
-
 import requests
-
 from google.oauth2 import id_token
-from google.auth.transport import requests as google_auth_requests
-
-from graphql_jwt.shortcuts import get_user_by_token, get_token, create_refresh_token
-
-
-def get_id_token_google(code):
-    client_secret_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'client_secret.json')
-
-    credentials = client.credentials_from_clientsecrets_and_code(
-        client_secret_path,
-        ['openid', 'email', 'profile', 'https://www.googleapis.com/auth/userinfo.profile',
-         'https://www.googleapis.com/auth/userinfo.email'],
-        code
-    )
-
-    return credentials.id_token
+import cachecontrol
+import google.auth.transport.requests
 
 
-def get_id_token_google_rest_api(code):
+# def get_id_token_google(code):
+#     client_secret_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'client_secret.json')
+#
+#     credentials = client.credentials_from_clientsecrets_and_code(
+#         client_secret_path,
+#         ['openid', 'email', 'profile', 'https://www.googleapis.com/auth/userinfo.profile',
+#          'https://www.googleapis.com/auth/userinfo.email'],
+#         code
+#     )
+#
+#     return credentials.id_token
+
+
+def get_google_id_token(code):
     token_url = 'https://oauth2.googleapis.com/token'
     token_data = {
         'code': code,
@@ -42,15 +38,12 @@ def get_id_token_google_rest_api(code):
 
     token_response_parsed = token_http_post.json()
 
-    id_info = id_token.verify_oauth2_token(token_response_parsed['id_token'], google_auth_requests.Request(),
-                                           settings.GOOGLE_CLIENT_ID)
+    session = requests.session()
+
+    cached_session = cachecontrol.CacheControl(session)
+
+    request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(token_response_parsed['id_token'], request, settings.GOOGLE_CLIENT_ID)
 
     return id_info
-
-
-def generate_token(user):
-    return get_token(user)
-
-
-def generate_refresh_token(user):
-    return create_refresh_token(user)
--- backend/api/views.py
+++ backend/api/views.py
@@ -7,9 +7,11 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .serializers import UserSerializer
-from .util import get_id_token_google_rest_api, generate_token, generate_refresh_token
+from .util import get_google_id_token
 from api.models import User
 import requests
+
+from graphql_jwt.shortcuts import get_token, create_refresh_token
 
 
 class GoogleLoginView(APIView):
@@ -17,10 +19,14 @@ class GoogleLoginView(APIView):
     def get(self, request):
         client_id = settings.GOOGLE_CLIENT_ID
         redirect_uri = settings.GOOGLE_REDIRECT_URI
+        scope = 'openid%20email%20profile'
+        access_type = "offline"
+
         auth_url = (
             f"https://accounts.google.com/o/oauth2/v2/auth?"
-            f"response_type=code&client_id={client_id}&"
-            f"redirect_uri={redirect_uri}&scope=openid%20email%20profile"
+            f"response_type=code&access_type={access_type}&"
+            f"redirect_uri={redirect_uri}&scope={scope}&"
+            f"client_id={client_id}"
         )
         return redirect(auth_url)
 
@@ -32,7 +38,7 @@ class GoogleAuthCallBackView(APIView):
             return Response({'error': 'Code is required'}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            details = get_id_token_google_rest_api(code)
+            details = get_google_id_token(code)
             email = details['email']
             email_verified = details['email_verified']
             first_name = details['given_name']
@@ -70,9 +76,9 @@ class GoogleAuthCallBackView(APIView):
                 else:
                     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-            token = generate_token(user)
+            token = get_token(user)
 
-            refresh_token = generate_refresh_token(user)
+            refresh_token = create_refresh_token(user)
 
             response = redirect(settings.CLIENT_ADDRESS)
 
