1712a5ed4c97296f8e8cda76f1a29dd48d2cb464
Mohamed Sharif <135287156+mSharifHub@users.noreply.github.com>
2024-08-04 02:32:21 -0700

    implemented authentication method 2

--- front-end/src/components/hooks/useUserLogin.ts
+++ front-end/src/components/hooks/useUserLogin.ts
@@ -1,15 +1,33 @@
 import { useSearchParams } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useFetchUser } from './useFetchUser.ts';
+import { useCallback, useEffect } from 'react';
+import { useLazyQuery } from '@apollo/client';
+import { ViewerQuery } from '../../graphql/types.ts';
+import { VIEWER_QUERY } from '../../graphql/queries/queries.ts';
+import Cookies from 'js-cookie';
+import { encryptData } from '../helpers/CookieEncryption.ts';
+import { useUser } from '../../userContext/UserContext.tsx';
 
 export const useUserLogin = () => {
   const [searchParams] = useSearchParams();
 
-  const fetchUser = useFetchUser();
+  const { dispatch } = useUser();
 
-  const handleFetchUser = async () => {
+  const [fetchUser] = useLazyQuery<ViewerQuery>(VIEWER_QUERY, {
+    onCompleted: (data) => {
+      if (data && data.viewer) {
+        dispatch({ type: 'SET_USER', payload: data.viewer });
+        const encryptedData = encryptData(data.viewer);
+        Cookies.set('user-meta-data', encryptedData, {
+          expiresIn: '3h',
+          sameSite: 'strict',
+        });
+      }
+    },
+  });
+
+  const handleFetchUser = useCallback(async () => {
     await fetchUser();
-  };
+  }, [fetchUser]);
 
   const redirectGoogleAuth = () => {
     window.location.href = import.meta.env.VITE_GOOGLE_RESOURCE_SERVER;
@@ -19,10 +37,10 @@ export const useUserLogin = () => {
     const success = searchParams.get('success');
 
     if (success === 'true') {
-      window.history.replaceState({}, document.title, '/');
       handleFetchUser();
+      window.history.replaceState({}, document.title, '/');
     }
-  }, [searchParams]);
+  }, [searchParams, handleFetchUser]);
 
   return { redirectGoogleAuth };
 };
