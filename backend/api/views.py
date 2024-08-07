7a6f8bc52b4471b3814e9c0b935e1e88a0db561d
Mohamed Sharif <135287156+mSharifHub@users.noreply.github.com>
2024-08-07 14:11:25 -0700

    implemented helper to get or update profile picture on login or logout

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,24 +1,19 @@
-import jwt
 from django.conf import settings
-from django.core.files.base import ContentFile
 from django.shortcuts import redirect
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
 from .util import get_google_id_token, generate_youtube_handler
 from api.models import User
-import requests
-from graphql_jwt.shortcuts import get_token, create_refresh_token, get_payload
-from graphql_jwt.utils import jwt_decode, get_user_by_payload
+from graphql_jwt.shortcuts import get_token, create_refresh_token
 from graphql_jwt import exceptions
 from .token_utils import revoke_refresh_token
-from django.contrib.auth import authenticate, logout as django_logout, login as django_login
+from django.contrib.auth import login as django_login
 from jwt import InvalidTokenError, InvalidIssuerError
 from django.views.decorators.debug import sensitive_variables
 from django.urls import reverse
-from django.contrib.sessions.models import Session
+from api.helpers import Helpers
 
 
 class GoogleLoginView(APIView):
@@ -40,10 +35,11 @@ class GoogleLoginView(APIView):
 class GoogleAuthCallBackView(APIView):
     @sensitive_variables('code', 'details', 'token', 'refresh_token')
     def get(self, request):
+        # 1 Gets the code from the query
         code = request.query_params.get('code', None)
         if not code:
             return Response({'error': 'Code is required'}, status=status.HTTP_400_BAD_REQUEST)
-
+        # 2 Gets the user data
         try:
             details = get_google_id_token(code)
             sub = details['sub']
@@ -53,13 +49,15 @@ class GoogleAuthCallBackView(APIView):
             last_name = details['family_name']
             profile_picture_url = details['picture']
 
+            # 3 Checks if user email is verified
             if not email_verified:
                 return Response({'error': 'Email verification required. Verify with Google before authenticating'},
-                                status=status.HTTP_400_BAD_REQUEST)
+                                status=status.HTTP_401_UNAUTHORIZED)
 
+            # 4 Checks if user already exists
             try:
-                user = User.objects.get(google_sub=sub)
-                user.is_verified = True
+                user = User.objects.get(email=email)
+                print(f'debugging user does exist {user}')
                 user.save()
 
             except User.DoesNotExist:
@@ -78,32 +76,24 @@ class GoogleAuthCallBackView(APIView):
 
                 if serializer.is_valid():
                     user = serializer.save()
-                    if profile_picture_url:
-                        response = requests.get(profile_picture_url)
-                        profile_picture = ContentFile(response.content)
-                        file_name = f"{first_name}_{last_name}_profile_picture.jpg"
-                        user.profile_picture.save(file_name, profile_picture, save=False)
                     user.save()
 
                 else:
                     return Response(serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-            authenticated_user = authenticate(request=request, google_sub=user.google_sub)
+            Helpers.get_profile_picture(user, first_name, last_name, profile_picture_url)
 
-            if authenticated_user is None:
-                return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-
-            # django_login(request, authenticated_user)
+            django_login(request, user, 'django.contrib.auth.backends.ModelBackend')
 
             extra_data = {
-                'google_sub': authenticated_user.google_sub,
-                'email': authenticated_user.email,
-                'youtube_handler': authenticated_user.youtube_handler,
+                'google_sub': user.google_sub,
+                'email': user.email,
+                'youtube_handler': user.youtube_handler,
             }
 
-            token = get_token(authenticated_user, **extra_data)
+            token = get_token(user, **extra_data)
 
-            refresh_token = create_refresh_token(authenticated_user)
+            refresh_token = create_refresh_token(user)
 
             response = redirect(f"{settings.CLIENT_ADDRESS}?success=true")
 
@@ -132,18 +122,14 @@ class GoogleAuthCallBackView(APIView):
 class LogoutView(APIView):
     def post(self, request):
 
-        # permission_classes = [IsAuthenticated]
-
         # session_key = request.COOKIES.get('sessionid')
         # session = Session.objects.get(session_key=session_key)
-        #
         # uid = session.get_decoded().get('_auth_user_id')
         # user = User.objects.get(google_sub=uid)
 
         if not request.user.is_authenticated:
             return Response({'success': False, 'error': 'Unauthorized or invalid request'},
                             status=status.HTTP_401_UNAUTHORIZED)
-
         try:
 
             refresh_token_value = request.COOKIES.get('JWT-refresh_token')
