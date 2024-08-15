d7c7ce583b7f7e33deb8884fa7019c1fec7218d7
Mohamed Sharif <135287156+mSharifHub@users.noreply.github.com>
2024-08-15 03:46:02 -0700

    Refactored
    implemented throttling for the window resize. it renders average 4 times from full width to compact width

---
+++ front-end/src/components/hooks/useThrottle.ts
@@ -0,0 +1,18 @@
+import { useRef } from 'react';
+
+export const useThrottle = <T extends (...args: any[]) => void>(
+  callBack: T,
+  interval: number,
+) => {
+  const lastExecuted = useRef<number>(0);
+
+  return (...args: Parameters<T>) => {
+    const now = Date.now();
+
+    if (now - lastExecuted.current < interval) return;
+
+    lastExecuted.current = now;
+
+    return callBack(...args);
+  };
+};
