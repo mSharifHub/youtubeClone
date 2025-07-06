8c82ab2a31b8a3057a55c21802a6c135c57f1b38
mohamedsharif <msharifemail@gmail.com>
2025-07-06 00:48:53 -0700

    implementing style and functionality to upload images

--- frontend/src/components/userComponent/UserChannel.tsx
+++ frontend/src/components/userComponent/UserChannel.tsx
@@ -1,7 +1,7 @@
 import { UserProfileCard } from './UserProfileCard.tsx';
 import { useUser } from '../../contexts/userContext/UserContext.tsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faX } from '@fortawesome/free-solid-svg-icons';
+import { faX, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 import { faImage } from '@fortawesome/free-regular-svg-icons';
 import React, { useEffect, useRef, useState } from 'react';
 
@@ -19,17 +19,29 @@ export default function UserChannel() {
   const [isDragging, setIsDragging] = useState<boolean>(false);
   const [imagePreviews, setImagePreviews] = useState<ImagePreviews[]>([]);
   const [error, setError] = useState<string | null>(null);
-  const fileInput = useRef<HTMLInputElement | null>(null);
+  const [previewsSize, setPreviewsSize] = useState<number>(0);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-  const MAX_SIZE = 10 * 1024 * 1024;
+  const MAX_MB = 10 * 1024 * 1024;
+
+  const MAX_UPLOADS = 10;
+
+  enum FileMimeType {
+    PNG = 'image/jpeg',
+    JPG = 'image/jpg',
+    JPEG = 'image/png',
+  }
+
+  const acceptable_mime_type = Object.values(FileMimeType) as string[];
+
   const handleUploadImageModal = (e: React.MouseEvent) => {
     e.stopPropagation();
     setShowUploadModal((prev) => !prev);
   };
 
   const handleDropZoneClick = () => {
-    if (fileInput.current) {
-      fileInput.current.click();
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
     }
   };
 
@@ -37,13 +49,13 @@ export default function UserChannel() {
     const previews = [...imagePreviews];
 
     const newFiles = Array.from(files).filter((file) => {
-      return file.type.match('image.*');
+      return file.size <= MAX_MB && acceptable_mime_type.includes(file.type) && file.size > 0;
     });
 
     let previousSize = previews.reduce((acc, curr) => acc + curr.file.size, 0);
     for (const file of newFiles) {
-      if (previousSize + file.size > MAX_SIZE) {
-        return setError('File size exceeds 10MB');
+      if (previousSize + file.size > MAX_MB || imagePreviews.length >= 10) {
+        return setError('Can not exceed 10MB or 10 pictures');
       }
       const url = URL.createObjectURL(file);
       previousSize += file.size;
@@ -53,6 +65,7 @@ export default function UserChannel() {
   };
 
   const handleFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.stopPropagation();
     if (e.target.files) {
       handleFiles(e.target.files);
       e.target.value = '';
@@ -80,11 +93,28 @@ export default function UserChannel() {
     setIsDragging(false);
   };
 
+  const handleRemoveFile = ({ index, event }: { index: number; event: React.MouseEvent<HTMLButtonElement> }) => {
+    event.stopPropagation();
+    setImagePreviews((prev) => {
+      if (prev.length === 0) return prev;
+      URL.revokeObjectURL(prev[index].url);
+      return prev.filter((_, idx) => idx !== index);
+    });
+  };
+
   useEffect(() => {
     if (imagePreviews) {
       console.log(imagePreviews);
     }
   }, [imagePreviews]);
+
+  useEffect(() => {
+    if (error) {
+      if (imagePreviews.length < MAX_MB) {
+        setError(null);
+      }
+    }
+  }, [imagePreviews.length]);
 
   useEffect(() => {
     return () => {
@@ -127,21 +157,53 @@ export default function UserChannel() {
               onDragOver={handleDragOver}
               onDragLeave={handleDragLeave}
               onClick={handleDropZoneClick}
-              className={`h-80  relative flex flex-col justify-center items-center  rounded-xl border   dark:border-neutral-600  bg-neutral-100 dark:bg-neutral-700 hover:cursor-pointer`}
+              className={` min-h-[400px] h-fit relative flex flex-col justify-center items-center  rounded-xl border   dark:border-neutral-600  bg-neutral-100 dark:bg-neutral-700 hover:cursor-pointer`}
             >
+              {error && <div>{error}</div>}
+
+              {imagePreviews.length > 0 && (
+                <div className="relative flex w-full aspect-square  p-8   justify-center items-center">
+                  {imagePreviews.map((preview, index) => {
+                    const rotateAngle = index % 2 === 0 ? 'rotate-3' : '-rotate-3';
+                    const closeButtonIndex = index % 2 === 0 ? 'right-0 top-0' : 'left-0 top-0 ';
+                    const zIndex = 10 + index;
+                    return (
+                      <div
+                        key={preview.file.name}
+                        className={`absolute ${rotateAngle} flex justify-center items-center h-[80%] w-[80%] max-h-[80%] max-w-[80%] `}
+                        style={{ zIndex }}
+                      >
+                        <div className="h-full w-full relative ">
+                          <button
+                            onClick={(event)=>handleRemoveFile({index,event})}
+                            className={` absolute  ${closeButtonIndex} h-10 w-10  `}>
+                            <FontAwesomeIcon icon={faX} className="text-2xl text-white " />
+                          </button>
+                          <img src={preview.url} alt={preview.file.name} className=" h-full w-full object-cover" />
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+
               {isDragging && <div className="absolute inset-0 rounded-lg bg-blue-500 opacity-50 w-full h-full" />}
 
               <div
                 onClick={handleUploadImageModal}
                 className="absolute -top-4 -right-2 h-10 w-10 flex justify-center items-center rounded-full  border    bg-white shadow  dark:border-0 dark:bg-neutral-900 cursor-pointer opacity-70"
               >
-                <FontAwesomeIcon icon={faX} className="text-xl font-thin" />
+                <FontAwesomeIcon icon={faMinus} className=" font-thin" />
               </div>
-              <input type="file" className="hidden" accept="image/*" multiple onChange={handleFileInputChange} ref={fileInput} />
-              <div className=" flex flex-col justify-center items-center gap-4">
-                <FontAwesomeIcon icon={faImage} className="text-4xl" />
-                <h3 className="text-sm">Click or drag image(s) from your computer</h3>
-              </div>
+
+              <input type="file" className="hidden" accept="image/*" multiple onChange={handleFileInputChange} ref={fileInputRef} />
+
+              {imagePreviews.length === 0 && (
+                <div className="flex flex-col  h-full w-full justify-center items-center gap-4 ">
+                  <FontAwesomeIcon icon={faImage} className="text-4xl" />
+                  <h3 className="text-xs text-center">Click on drop area to drag image(s) or select from your computer</h3>
+                </div>
+              )}
             </div>
           )}
 
